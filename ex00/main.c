977e5d84489c3e51ed23b8d6c0bae45ee35f5153
Sen Li <seli@e1z1r7p16.42.us.org>
2018-09-30 21:44:17 -0700

    remove printf

--- ex00/main.c
+++ ex00/main.c
@@ -6,11 +6,10 @@
 /*   By: seli <seli@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/30 20:41:02 by seli              #+#    #+#             */
-/*   Updated: 2018/09/30 21:20:09 by seli             ###   ########.fr       */
+/*   Updated: 2018/09/30 21:43:31 by seli             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
 #include <unistd.h>
 #include "ft_imperative_sudoku.h"
 
@@ -22,11 +21,7 @@ int		main(int ac, char **av)
 	int	matrix[9][9][10] = {{{0}}};
 
 	create_board(ac, av, board);
-	print_board(board);
-	printf("\n");
 	ft_imperative_sudoku(board, matrix);
-	print_board(board);
-	printf("\n");
 	backtrack(board, solved_board, matrix, &solutions);
 	if (solutions == 1)
 		print_board(solved_board);
