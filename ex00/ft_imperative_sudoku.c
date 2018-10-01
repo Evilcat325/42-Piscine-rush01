0159b752783af7899104a1e7ca1cc4ba98d95ec1
Sen Li <seli@e1z1r7p16.42.us.org>
2018-09-30 19:48:36 -0700

    imperative norm fix

--- ex00/ft_imperative_sudoku.c
+++ ex00/ft_imperative_sudoku.c
@@ -6,7 +6,7 @@
 /*   By: seli <seli@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/29 13:40:44 by seli              #+#    #+#             */
-/*   Updated: 2018/09/30 19:22:26 by seli             ###   ########.fr       */
+/*   Updated: 2018/09/30 19:48:10 by seli             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,116 +30,6 @@ void	ft_imperative_sudoku(int board[9][9], int matrix[9][9][10])
 			ft_row(board, matrix, number, &updated);
 			ft_col(board, matrix, number, &updated);
 			number++;
-		}
-	}
-}
-
-void	ft_row(int b[9][9], int m[9][9][10], int number, int *updated)
-{
-	int	row;
-	int col;
-	int	count;
-
-	row = -1;
-	while (++row < 9)
-	{
-		col = -1;
-		count = 0;
-		while (++col < 9)
-			if (m[row][col][number] == POSSIBLE)
-				count++;
-		col = -1;
-		if (count != 1)
-			continue;
-		while (++col < 9)
-		{
-			if (m[row][col][number] == POSSIBLE)
-			{
-				b[row][col] = number;
-				ft_update(b, m, row, col);
-				*updated = TRUE;
-			}
-		}
-	}
-}
-
-void	ft_col(int b[9][9], int m[9][9][10], int number, int *updated)
-{
-	int	row;
-	int col;
-	int	count;
-
-	col = -1;
-	while (++col < 9)
-	{
-		row = -1;
-		count = 0;
-		while (++row < 9)
-			if (m[row][col][number] == POSSIBLE)
-				count++;
-		row = -1;
-		if (count != 1)
-			continue;
-		while (++row < 9)
-		{
-			if (m[row][col][number] == POSSIBLE)
-			{
-				b[row][col] = number;
-				ft_update(b, m, row, col);
-				*updated = TRUE;
-			}
-		}
-	}
-}
-
-void	ft_box(int b[9][9], int m[9][9][10], int number, int *updated)
-{
-	int	i;
-	int j;
-	int	count;
-
-	i = -1;
-	while (++i < 9)
-	{
-		j = -1;
-		count = 0;
-		while (++j < 9)
-			if (m[BOXR(i, j)][BOXC(i, j)][number] == POSSIBLE)
-				count++;
-		j = -1;
-		if (count != 1)
-			continue;
-		while (++j < 9)
-		{
-			if (m[BOXR(i, j)][BOXC(i, j)][number] == POSSIBLE)
-			{
-				b[BOXR(i, j)][BOXC(i, j)] = number;
-				ft_update(b, m, BOXR(i, j), BOXC(i, j));
-				*updated = TRUE;
-			}
-		}
-	}
-}
-
-void	ft_cell(int b[9][9], int m[9][9][10], int *updated)
-{
-	int i;
-	int number;
-
-	i = -1;
-	while (++i < 81)
-	{
-		if (b[i / 9][i % 9] != 0
-			|| m[i / 9][i % 9][0] + 1 != 9)
-			continue;
-		number = 0;
-		while (++number <= 9)
-		{
-			if (m[i / 9][i % 9][number] != POSSIBLE)
-				continue;
-			b[i / 9][i % 9] = number;
-			ft_update(b, m, i / 9, i % 9);
-			*updated = TRUE;
 		}
 	}
 }
